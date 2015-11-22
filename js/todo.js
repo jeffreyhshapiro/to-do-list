42ab3c242f11a653058b404233a3e3a98a9bb1eb
unknown <jeffreyhshapiro@gmail.com>
2015-11-21 22:39:50 -0500

    began working on appending delete button to to do list

--- js/todo.js
+++ js/todo.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
 
   //Add a task to the second column of the to do table
   
-    alert("why u almost working")
     var taskItem = $("<td>").append(taskToDo);
     var taskToDo = $("#comment").val();
     var newRow = $("<tr>");
+    var deleteBtn = $("<button>").addClass("btn btn-danger").append("X");
     $("#to-do-list").append(newRow);
       newRow.append("<td>X</td>")
       newRow.append('<td>'+taskToDo+'</td>');
-      console.log(taskItem);
-
+      newRow.append('<td>'+deleteBtn+'</td>');
+      
 
 
   });
